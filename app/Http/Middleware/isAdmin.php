bc63533dcea2b8f2f243e705e8904dde56ee7cb3
Azkia <azisarf9@gmail.com>
2019-06-07 00:16:53 +0800

    bootstrap template+auth

---
+++ app/Http/Middleware/isAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Admin;
+
+class isAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $dosen = Pic::find(Auth::user()->username);
+        if($dosen)
+            return $next($request);
+        
+        return response()->view('errors.401', [], 401);
+    }
+}
