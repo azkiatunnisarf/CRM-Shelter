3b45021b61ddac24b2820e7b32a75139f8e0fe9b
Azkia <azisarf9@gmail.com>
2019-06-30 09:46:06 +0700

    filter by month

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,7 +62,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'is_admin' => \App\Http\Middleware\IsAdmin::class,
-        'is_officer' => \App\Http\Middleware\IsAdmin::class,
+        'is_officer' => \App\Http\Middleware\IsOfficer::class,
         'is_manager_crm' => \App\Http\Middleware\IsAdmin::class,
         'is_manager_non_crm' => \App\Http\Middleware\IsAdmin::class,
         'is_direktur' => \App\Http\Middleware\IsAdmin::class,
